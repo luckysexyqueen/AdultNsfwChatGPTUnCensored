fd98329f596f0d3f7ed62a9b67c24d1711322195
LuckySexySuccubusQueen135Age <84852627+luckysexyqueen@users.noreply.github.com>
2026-08-13 14:35:06 +0900

    Add files via upload

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,7 @@
-// Utility function for combining class names
-export function cn(...inputs: (string | undefined | null | false)[]) {
-  return inputs.filter(Boolean).join(' ');
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+/** Tailwind 충돌을 정리하면서 조건부 클래스 값을 안전하게 결합합니다. */
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
 }
